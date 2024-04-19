585b488f73a515e206530c27fbf3c495d197d999
SurjeetMandal <surjeetmandal698@gmail.com>
2024-04-19 17:54:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-//your code here
+function allowDrop(event) {
+  event.preventDefault();
+}
+
+function drag(event) {
+  event.dataTransfer.setData('text', event.target.id);
+}
+
+function drop(event) {
+  event.preventDefault();
+  const data = event.dataTransfer.getData('text');
+  event.target.parentNode.insertBefore(document.getElementById(data), event.target);
+}
+
+const images = document.querySelectorAll('.image');
+images.forEach(image => {
+  image.addEventListener('dragstart', drag);
+  image.addEventListener('dragover', allowDrop);
+  image.addEventListener('drop', drop);
+});
+
